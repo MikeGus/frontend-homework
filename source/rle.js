8ccb097bbd5b8af45cc30b796fdb23a41a8e26f5
MikeGus <vfc17@yandex.ru>
2017-08-29 22:08:35 +0300

    added tests for empty string and string without repetition

--- source/rle.js
+++ source/rle.js
@@ -1,6 +1,9 @@
 const rle = function(string) {
 	var retString = "";
 	var matchArray = string.match(/([a-zA-Z])\1*/ig);
+	if (matchArray == null) {
+		return "";
+	}
 	matchArray.forEach(function(item) {
 		retString += item[0];
 		if (item.length > 1) {
---
+++ tests/rle.js
@@ -0,0 +1,15 @@
+'use strict';
+
+QUnit.module('Тестируем функцию rle', function () {
+	QUnit.test('rle работает правильно', function (assert) {
+		assert.strictEqual(rle('AAAB'), 'A3B');
+		assert.strictEqual(rle('BCCDDDAXXXX'), 'BC2D3AX4');
+		assert.strictEqual(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
+	});
+	QUnit.test('rle работает правильно на пустых строках', function (assert) {
+		assert.strictEqual(rle(''), '');
+	});
+	QUnit.test('rle работает правильно на строках без повторяющихся символов', function (assert) {
+		assert.strictEqual(rle('ABCDEFGHZ'), 'ABCDEFGHZ');
+	});
+});
