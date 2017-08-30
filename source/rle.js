313802ce1fdc22d6e7a3ac08495f4906751a1dcb
MikeGus <vfc17@yandex.ru>
2017-08-30 22:11:13 +0300

    simplified code with arrow synt

--- source/rle.js
+++ source/rle.js
@@ -1,12 +1,9 @@
 'use strict';
-const rle = function(string) {
-	let retString = '';
+const rle = string => {
 	const matchArray = string.match(/([\wа-яА-Я])\1*/ig);
 	if (matchArray === null) {
 		return '';
 	}
-	retString = matchArray.reduce(function(retString, item) {
-		return  `${retString}${item[0]}${item.length > 1 ? item.length : ''}`;
-	}, '');
-	return retString;
+	return matchArray.reduce((retString, item) => 
+		`${retString}${item[0]}${item.length > 1 ? item.length : ''}`, '');
 }
